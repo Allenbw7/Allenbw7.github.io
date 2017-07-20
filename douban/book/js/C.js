5243a738e0fa89182e054fb8be14528b65210483
allen <allenbw7@gamil.com>
2017-07-20 22:43:06 +0800

    1.2

--- douban/book/js/C.js
+++ douban/book/js/C.js
@@ -3,17 +3,22 @@
 const $btn = $('.inp-btn input'),
       $txt = $('#inp-query');
 //初始页码
-let num = 0,
-    n = 0,
-    val = '';
+let num = 0,//start从第num个数据开始显示
+    n = 0,//页码
+    sortMode = 1,//排序模式，1默认，2时间，3评分
+    val = '';//要搜索的数据名称
 
 //初始化，显示javascript搜索结果书籍
 Modle('javascript',num,n);
+//初始化，抓取所有数据存到本地待调用
+ModleAll('javascript',num,n);
 
 //点击搜索，按照输入内容请求数据并展示
 $btn.click(function() {
+  num = n = 0;
   val = $txt.val();
-  Modle(val,0,0);
+  Modle(val,num,n);//搜索后首次数据展示
+  ModleAll(val,num,n);//拉取搜索匹配条目到本地
   return false;
 })
 
@@ -27,18 +32,69 @@ $pageA.on('click','a',function(ev) {
   num = (ev.target.innerHTML-1)*49;
   n = ev.target.innerHTML-1;
   val = val || 'javascript';
-  Modle(val,num,n);
+  changePage();
 })
 
 //翻页，上一页
-$prev.click('a',function() {
-  n--;
-  Modle(val,num,n);
+$prev.click(function(ev) {
+  if(n>0) {
+    num -= 49;
+    n--;
+    changePage();
+  }
 })
 
 //翻页，下一页
-$next.click('a',function() {
+$next.on('click','a',function(ev) {
+  num += 49;
   n++;
   val = val || 'javascript';
-  Modle(val,num,n);
+  changePage();
+  return false;
 })
+
+//根据排序模式渲染数据
+function changePage() {
+  switch (sortMode) {
+    case 1:
+      Modle1(val,num,n);
+      break;
+    case 2:
+      Modle2(val,num,n);
+      break;
+    case 3:
+      Modle3(val,num,n);
+      break;
+    default:
+      break;
+  }
+}
+
+//搜索结果排序1，传参调用函数
+for(let i=0; i<3; i++) {
+  $('.bookSort a').eq(i).click(function() {
+    bookS(i);
+    $(this).addClass('deSort').parent().siblings().children().removeClass('deSort');
+  });
+}
+
+//搜索结果排序2，0默认排序，1出版时间排序，2评分排序
+function bookS(i) {
+  val = val || 'javascript';
+  switch (i) {
+    case 0:
+      Modle1(val,num,n);
+      sortMode = 1;
+      break;
+    case 1:
+      Modle2(val,num,n);
+      sortMode = 2;
+      break;
+    case 2:
+      Modle3(val,num,n);
+      sortMode = 3;
+      break;
+    default:
+      break;
+  }
+}
--- douban/book/js/M.js
+++ douban/book/js/M.js
@@ -1,3 +1,57 @@
+let bookAll = {books:[]},//默认排序
+    bookAll1 = {books:[]},//时间排序
+    bookAll2 = {books:[]};//评分排序
+
+//向豆瓣接口请求搜索内容的具体数据
+function ModleAll(val,num,n){
+  $.ajax({
+ 		url:'https://api.douban.com/v2/book/search?callback=?',
+    dataType:'jsonp',
+    data:{
+      q:val,
+      start:num,
+      count:100,
+    },
+    success:function(data){
+      if(n == 0) {
+        bookAll = {books:[]};
+      }
+      for(let i=0; i<data.books.length; i++) {
+        bookAll.books.push(data.books[i]);
+        n++;
+      }
+      num += 100;
+      if(n < data.total) {
+        ModleAll(val,num,n);
+      }else {
+        bookAll1 = extend(bookAll);
+        bookAll2 = extend(bookAll);
+        bookAll1.books.sort(function(a,b) {
+          return getDate(b.pubdate) - getDate(a.pubdate);
+        });
+        bookAll2.books.sort(function(a,b) {
+          return b.rating.average - a.rating.average;
+        });
+        console.log(bookAll,bookAll1,bookAll2);
+      }
+    }
+  });
+}
+
+//深度克隆数组/对象
+function extend(obj){
+  var o = Object.prototype.toString.call(obj) === '[object Array]'?[]:{};
+  for(var attr in obj){
+    if(typeof obj[attr] === 'object'){
+      o[attr] = extend( obj[attr] );
+    }else{
+      o[attr] = obj[attr];
+    }
+  }
+  return o;
+}
+
+
 //向豆瓣接口请求搜索内容的具体数据
 function Modle(val,num,n){
   $.ajax({
@@ -9,9 +63,10 @@ function Modle(val,num,n){
       count:49,
     },
     success:function(data){
-    	console.log(data,n)
+    	console.log(data)
       data.len = Math.ceil(data.total/49);
-      $('#bookList').html(View(data));
+      $('.trr').html(`<h5 class="trr">搜索结果${data.start+1}-${data.start+49},共${data.total}</h5>`);
+      $('#bookList').html(View(data,num));
       $('#page').html(Page(data,n));
       scrollTo(0,0);
       pn(n,data.len-1);
@@ -20,15 +75,142 @@ function Modle(val,num,n){
 }
 
 
+//按时间排序，新拉取数据
+// function Modle1(val,num,n){
+//   $.ajax({
+//  		url:'https://api.douban.com/v2/book/search?callback=?',
+//     dataType:'jsonp',
+//     data:{
+//       q:val,
+//       start:num,
+//       count:49,
+//     },
+//     success:function(data){
+//     	//console.log(data)
+//       data.books.sort(function(a,b) {
+//         return getDate(b.pubdate) - getDate(a.pubdate);
+//       })
+//       //console.log(data)
+//       data.len = Math.ceil(data.total/49);
+//       $('.trr').html(`<h5 class="trr">搜索结果${data.start+1}-${data.start+49},共${data.total}</h5>`);
+//       $('#bookList').html(View(data));
+//       $('#page').html(Page(data,n));
+//       scrollTo(0,0);
+//       pn(n,data.len-1);
+//     }
+//   });
+// }
+
+
+//按评分排序，新拉取数据
+// function Modle2(val,num,n){
+//   $.ajax({
+//  		url:'https://api.douban.com/v2/book/search?callback=?',
+//     dataType:'jsonp',
+//     data:{
+//       q:val,
+//       start:num,
+//       count:49,
+//     },
+//     success:function(data){
+//     	//console.log(data)
+//       data.books.sort(function(a,b) {
+//         return b.rating.average - a.rating.average;
+//       })
+//       data.len = Math.ceil(data.total/49);
+//       $('.trr').html(`<h5 class="trr">搜索结果${data.start+1}-${data.start+49},共${data.total}</h5>`);
+//       $('#bookList').html(View(data));
+//       $('#page').html(Page(data,n));
+//       scrollTo(0,0);
+//       pn(n,data.len-1);
+//     }
+//   });
+// }
+
+
+//按默认排序，缓存数据
+function Modle1(val,num,n){
+
+      // bookAll.books.sort(function(a,b) {
+      //   return getDate(b.pubdate) - getDate(a.pubdate);
+      // })
+      bookAll.len = Math.ceil(bookAll.books.length/49);
+      $('.trr').html(`<h5 class="trr">搜索结果${num+1}-${num+49},共${bookAll.books.length}</h5>`);
+      $('#bookList').html(View(bookAll,num));
+      $('#page').html(Page(bookAll,n));
+      scrollTo(0,0);
+      pn(n,bookAll.len-1);
+
+}
+
+
+//按时间排序，缓存数据
+function Modle2(val,num,n){
+
+      // bookAll.books.sort(function(a,b) {
+      //   return getDate(b.pubdate) - getDate(a.pubdate);
+      // })
+      bookAll1.len = Math.ceil(bookAll1.books.length/49);
+      $('.trr').html(`<h5 class="trr">搜索结果${num+1}-${num+49},共${bookAll1.books.length}</h5>`);
+      $('#bookList').html(View(bookAll1,num));
+      $('#page').html(Page(bookAll1,n));
+      scrollTo(0,0);
+      pn(n,bookAll1.len-1);
+
+}
+
+
+//按评分排序，缓存数据
+function Modle3(val,num,n){
+
+    	//console.log(data)
+      // bookAll.books.sort(function(a,b) {
+      //   return b.rating.average - a.rating.average;
+      // })
+      bookAll2.len = Math.ceil(bookAll2.books.length/49);
+      $('.trr').html(`<h5 class="trr">搜索结果${num+1}-${num+49},共${bookAll2.books.length}</h5>`);
+      $('#bookList').html(View(bookAll2,num));
+      $('#page').html(Page(bookAll2,n));
+      scrollTo(0,0);
+      pn(n,bookAll2.len-1);
+
+}
+
+
 //当选中第一页和最后一页时，对应上一页或下一页不可点击状态
 function pn(n,len) {
-  console.log(n,len)
+  //console.log(n,len)
   if(n == 0) {
     $('.prev').html('上一页');
+    $('.prev').attr('onOff',false);
   }else if(n == len) {
     $('.next').html('下一页');
+    $('.next').attr('onOff',false);
   }else {
     $('.prev').html('<a href="javascript:;"><<上一页</a>');
     $('.next').html('<a href="javascript:;">下一页>></a>');
   }
+}
+
+//转换时间格式
+function getDate(data) {
+  let time = data.split('-');
+  if(time.length == 0) {
+    time = '00';
+  }else if(time.length == 1) {
+    time = time[0];
+  }else if(time.length == 2) {
+    if(time[1].length == 1) {
+      time[1] = '0' + time[1];
+    }
+    time = time[0] + time[1] + '00';
+  }else if(time.length == 3) {
+    for(let i=1; i<time.length; i++) {
+      if(time[i].length == 1) {
+        time[i] = '0' + time[i];
+      }
+    }
+    time = (time[0]+time[1]+time[2]);
+  }
+  return time*1;
 }
